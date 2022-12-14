811762e9bea49dfd265645a884a3bf7e8004b149
Emil <emil88pl@gmail.com>
2022-12-14 12:08:18 +0000

    Add ==

--- src/main/java/com/example/demo/Time.java
+++ src/main/java/com/example/demo/Time.java
@@ -4,26 +4,8 @@ package com.example.demo;
 public class Time {
     public static void main(String[] args) {
         int time;
-
-        // time = 2;
         
-        Gettime(2);
-
-    //   if (time <= 11) {
-    //     System.out.println("Cost of booking will be: " + "£" + time*2); // per minute
-    //   } else if (time > 11 && time <= 60) {
-    //     System.out.println("Cost of booking will be: " + "£" + 22); // per hour
-    // } else if (time > 61 && time <= 120) {
-    //     System.out.println("Cost of booking will be: " + "£" + 44); // 2h 
-    // } else if (time > 120 && time <= 180) {
-    //     System.out.println("Cost of booking will be: " + "£" + 60); // 3h up - £6
-    // } else if (time > 181 && time < 1440) {
-    //     System.out.println("Cost of booking will be: " + "£" + 60); // per day
-    //   } else if (time >= 1440 && time < 2880) {
-    //     System.out.println("Cost of booking will be: " + "£" + 105); // per week
-    //   } else if (time >= 2880) {
-    //     System.out.println("Cost of booking will be: " + "£" + 210); 
-    //   }
+        Gettime(181);
     }
 
     public static void Gettime(int time) {
@@ -32,13 +14,13 @@ public class Time {
       } else if (time > 11 && time <= 60) {
             System.out.println("Cost of booking will be: " + "£" + 22); // per hour
       }
-      else  if (time > 61 && time <= 120) {
+      else  if (time >= 61 && time <= 120) {
             System.out.println("Cost of booking will be: " + "£" + 44); // 2h 
       }
-       else if (time > 120 && time <= 180) {
+        else if (time > 120 && time <= 180) {
             System.out.println("Cost of booking will be: " + "£" + 60); // 3h up - £6
       }
-       else if (time > 181 && time < 1440) {
+       else if (time >= 181 && time < 1440) {
             System.out.println("Cost of booking will be: " + "£" + 60); // per day
       }
       else  if (time >= 1440 && time < 2880) {

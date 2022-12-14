16b5adf6f93cc0cebadfe81156cd12be2c28cec0
Emil <emil88pl@gmail.com>
2022-12-14 12:03:01 +0000

    added Method

--- src/main/java/com/example/demo/Time.java
+++ src/main/java/com/example/demo/Time.java
@@ -4,22 +4,45 @@ package com.example.demo;
 public class Time {
     public static void main(String[] args) {
         int time;
-        time = 360;
 
-      if (time <= 11) {
-        System.out.println("Cost of booking will be: " + "£" + time*2); // per minute
-      } else if (time > 11 && time <= 60) {
-        System.out.println("Cost of booking will be: " + "£" + 22); // per hour
-    } else if (time > 61 && time <= 120) {
-        System.out.println("Cost of booking will be: " + "£" + 44); // 2h 
-    } else if (time > 120 && time <= 180) {
-        System.out.println("Cost of booking will be: " + "£" + 60); // 3h up - £6
-    } else if (time > 181 && time < 1440) {
-        System.out.println("Cost of booking will be: " + "£" + 60); // per day
-      } else if (time >= 1440 && time < 2880) {
-        System.out.println("Cost of booking will be: " + "£" + 105); // per week
-      } else if (time >= 2880) {
-        System.out.println("Cost of booking will be: " + "£" + 210); 
-      }
+        // time = 2;
+        
+        Gettime(2);
+
+    //   if (time <= 11) {
+    //     System.out.println("Cost of booking will be: " + "£" + time*2); // per minute
+    //   } else if (time > 11 && time <= 60) {
+    //     System.out.println("Cost of booking will be: " + "£" + 22); // per hour
+    // } else if (time > 61 && time <= 120) {
+    //     System.out.println("Cost of booking will be: " + "£" + 44); // 2h 
+    // } else if (time > 120 && time <= 180) {
+    //     System.out.println("Cost of booking will be: " + "£" + 60); // 3h up - £6
+    // } else if (time > 181 && time < 1440) {
+    //     System.out.println("Cost of booking will be: " + "£" + 60); // per day
+    //   } else if (time >= 1440 && time < 2880) {
+    //     System.out.println("Cost of booking will be: " + "£" + 105); // per week
+    //   } else if (time >= 2880) {
+    //     System.out.println("Cost of booking will be: " + "£" + 210); 
+    //   }
     }
+
+    public static void Gettime(int time) {
+      if (time <= 6) {
+        System.out.println("Cost of booking will be: " + "£" + time*2); // per minut
+      } else if (time > 11 && time <= 60) {
+            System.out.println("Cost of booking will be: " + "£" + 22); // per hour
+      }
+      else  if (time > 61 && time <= 120) {
+            System.out.println("Cost of booking will be: " + "£" + 44); // 2h 
+      }
+       else if (time > 120 && time <= 180) {
+            System.out.println("Cost of booking will be: " + "£" + 60); // 3h up - £6
+      }
+       else if (time > 181 && time < 1440) {
+            System.out.println("Cost of booking will be: " + "£" + 60); // per day
+      }
+      else  if (time >= 1440 && time < 2880) {
+            System.out.println("Cost of booking will be: " + "£" + 105); // per week
+      }
+}   
 }

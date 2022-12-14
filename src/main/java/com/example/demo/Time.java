83989534b682db539ada9bfaf76876963a90bc3a
Emil <emil88pl@gmail.com>
2022-12-14 13:42:30 +0000

    comments

--- src/main/java/com/example/demo/Time.java
+++ src/main/java/com/example/demo/Time.java
@@ -11,22 +11,22 @@ public class Time {
     public static int GetPrice(int time) {
       if (time <= 6) {
         return time*2; // per minute
-      } else if (time > 11 && time <= 60) {
+      } else if (time >= 11 && time <= 60) {
            return 22; // per hour
       }
       else  if (time >= 61 && time <= 120) {
             return 44; // 2h 
       }
-        else if (time > 120 && time <= 180) {
+        else if (time >= 121 && time <= 180) {
            return 60; // 3h up - £6 use daily price of £60
       }
-       else if (time >= 181 && time < 1440) {
+       else if (time >= 181 && time <= 1440) {
            return 60; // per day
       }
-      else  if (time >= 1440 && time < 2880) {
+      else if (time >= 1441 && time <= 2880) {
            return 105; // per week
       }
-      else  if (time >= 2880 && time < 9999) {
+      else if (time >= 2881 && time < 99999) {
        return 210; // more than week
       }
     return time;
